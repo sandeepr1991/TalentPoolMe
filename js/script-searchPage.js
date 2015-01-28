fa3fa3b67203ee21b6be07515455812789bafa5b
Laurence-USC <olivier_bbs@outlook.com>
2015-01-28 15:23:11 -0800

    Search page modified
    Add hyper links to search page and modify the filter part.

--- js/script-searchPage.js
+++ js/script-searchPage.js
@@ -24,14 +24,65 @@ var companies = ['4A\'s', 'ABB Group', 'Accenture', 'Burlington Stores',
 'EOG Resources, Inc. (formerly Enron Oil and Gas)' ,'Exelon Corporation', 'FCA Group - Fiat Chrysler Auto Group', 
 'Marriott International ', 'Mars Incorporated', 'Moss Adams LLP', 'Oracle', 'SABIC Innovative Plastics', 
 'Siemens', 'Sprint', 'TCS (Tata Consultancy Services)', 'TRW Automotive Holdings Corp.'];
- 
-$('.typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'companies',
-  displayKey: 'value',
-  source: substringMatcher(companies)
+
+var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", 
+"Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
+"Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", 
+"North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
+"Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+
+var EVPs = ["Attractive/exciting products and services", "Challenging work", "Clear path for advancement", "Client interaction", 
+"Competitive base salary", "Competitive benefits", "Control over my number of working hours", "Corporate Social Responsibility", 
+"Creative work environment", "Enabling talent to integrate personal Interests into schedule", "Environmental sustainability", 
+"Ethical standards", "Fast-growing/entrepreneurial", "Financial strength", "Flexible working conditions", 
+"Friendly work environment", "Good reference for future career", "High future earnings", "High level of responsibility", "Innovation", 
+"Inspiring management", "Interaction with international clients and colleagues", "Leaders who will support my development", 
+"Leadership opportunities", "Market success", "Opportunities for international travel/relocation", "Overtime pay/compensation", 
+"Performance-related bonus", "Prestige", "Professional training and development", "Rapid promotion", 
+"Recognizing performance (meritocracy)", "Recruiting only the best talent", "Respect for diversity", "Respect for its people", 
+"Secure employment", "Sponsorship of future education", "Support for gender equality", "Team-oriented work", "Variety of assignments", 
+"Other, please specify: (Fill in)"];
+
+$(function() {
+	$('.typeahead').typeahead({
+		hint: true,
+		highlight: true,
+		minLength: 1
+	},
+	{
+		name: 'companies',
+		displayKey: 'value',
+		source: substringMatcher(companies)
+	});
+	
+	
+	for(i = 0; i < companies.length; i++) {
+		$('#filterOption-industry').append("<option value=\"" + companies[i] + "\">" + companies[i] + "</option>");
+	}
+	
+	for(i = 0; i < states.length; i++) {
+		$('#filterOption-state').append("<option value=\""+ states[i] + "\">" + states[i] + "</option>");
+	}
+	
+	for(i = 0; i < EVPs.length - 1; i++) {
+		$('#filterOption-EVP').append("<option value=\""+ EVPs[i] + "\">" + EVPs[i] + "</option>");
+	}
+	
+	$('#filterOption-EVP').append("<option value=\"other\">" + EVPs[i] + "</option>");
+	$('#filterOption-EVP').change(function() {
+		if($('#filterOption-EVP option:selected').attr('value') === "other") {
+			$('#filters').append("<tr id=\"EVPSpec\"><td><h4>EVP specify: </h4></td><td><input type=\"text\" class=\"form-control\"/></td></tr>");
+		}
+		
+		if($('#filterOption-EVP option:selected').attr('value') !== "other") {
+			$('#EVPSpec').remove();
+		}
+	});
 });
+
+
+
+
+
+
+
